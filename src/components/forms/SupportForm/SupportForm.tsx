d29f4e55fd017a0c14fa60a735645f916a434342
David De Los Santos <ddelossantosmateo@gmail.com>
2020-10-17 14:34:07 -0400

    Working on support form input
    And creating support form story

--- src/components/forms/SupportForm/SupportForm.tsx
+++ src/components/forms/SupportForm/SupportForm.tsx
@@ -3,10 +3,10 @@ import './SupportForm.css'
 import {Card, Input, Button} from 'antd'
 const SupportForm: React.FunctionComponent<any> = (props) => {
     return(
-        <div className="formContainer">
-            <Card>
-                <p className="nameTextContainer">Nombre</p>
-                <Input className="nameFormContainer" placeholder="Introduzca su nombre..."/>
+        <div>
+            <Card className="formContainer">
+                <p>Nombre</p>
+                <Input placeholder="Introduzca su nombre..."/>
             </Card>
         </div>
     )
--- src/stories/SupportForm/SupportForm.stories.tsx
+++ src/stories/SupportForm/SupportForm.stories.tsx
@@ -1,8 +1,12 @@
 import {storiesOf} from '@storybook/react'
 import * as React from 'react'
 import SupportText from '../../components/forms/SupportForm/SupportText'
-
+import SupportForm from '../../components/forms/SupportForm/SupportForm'
 storiesOf('Support Form', module)
     .add('Support Form Title', () => (
         <SupportText/>
+    ))
+
+    .add('Support Form', () => (
+        <SupportForm/>
     ))
