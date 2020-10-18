0029c480c57c766d44ee013aece0ea6a04b9d994
brianf.baez8@gmail.com <brianf.baez8@gmail.com>
2020-10-17 20:05:00 -0400

    Updates

---
+++ precache-manifest.6cd657a15d9cd538ba7d14017d8ef15b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63bafc4ad76db474247865aafb7b55e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c6a1f9068749f0c1c7d",
+    "url": "/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "172d35728183f205a93c",
+    "url": "/static/css/main.8bb83e72.chunk.css"
+  },
+  {
+    "revision": "9c6a1f9068749f0c1c7d",
+    "url": "/static/js/2.a88ffb08.chunk.js"
+  },
+  {
+    "revision": "c1e665248f04cd3849d99073e416e494",
+    "url": "/static/js/2.a88ffb08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "172d35728183f205a93c",
+    "url": "/static/js/main.18b6ecf7.chunk.js"
+  },
+  {
+    "revision": "c66366b65dce111e4c06",
+    "url": "/static/js/runtime-main.c62e7cce.js"
+  },
+  {
+    "revision": "164f727014377b6c0c88f783ae3a29ff",
+    "url": "/static/media/kitao-logo.164f7270.png"
+  }
+]);
